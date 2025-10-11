0c234526dd75002ef6ce39275b4d8122416a9bcb
Atomic Sorcerer <67711756+atomicsorcerer@users.noreply.github.com>
2025-10-10 23:15:06 -0700

    Updated raw data processing to use Pandas, as well. Uploaded data for intermediate processing step.

--- data/process_raw_data.py
+++ data/process_raw_data.py
@@ -1,24 +1,36 @@
-import polars as pl
+import pandas as pd
 import numpy as np
 
 
 def extract_data(file_path: str):
-	with open(file_path, "r") as raw_data:
-		data = []
-		
-		for line in raw_data.readlines():
-			data.append(list(map(float, line.strip().split(" "))))
-			
-		return data
+    with open(file_path, "r") as raw_data:
+        data = []
+
+        for line in raw_data.readlines():
+            data.append(list(map(float, line.strip().split(" "))))
+
+        return data
 
 
-labels = ["mass", "pT_1", "pT_2", "px_1", "py_1", "pz_1", "energy_1", "px_2", "py_2", "pz_2", "energy_2"]
+labels = [
+    "mass",
+    "pT_1",
+    "pT_2",
+    "px_1",
+    "py_1",
+    "pz_1",
+    "energy_1",
+    "px_2",
+    "py_2",
+    "pz_2",
+    "energy_2",
+]
 
 signal = extract_data("signal (raw)/zpjj_signal-8D-mmjj400.0-mzp1000.0.txt")
-csv_signal_data = pl.DataFrame(signal, schema=labels, orient="row")
+csv_signal_data = pd.DataFrame(signal, columns=labels)
 csv_signal_data.write_csv("signal.csv")
 
 
 background = extract_data("background (raw)/ppuu_background-8D-mmjj400.txt")
-csv_background_data = pl.DataFrame(background, schema=labels, orient="row")
+csv_background_data = pd.DataFrame(background, columns=labels)
 csv_background_data.write_csv("background.csv")
